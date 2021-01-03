30d071674afd3af4036d61efab828e410ec16d44
vardhan2 <68744020+vardhan2@users.noreply.github.com>
2021-01-03 20:29:41 +0530

    changes 5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-constElement = <p>hello4</p>;
+constElement = <p>hello5</p>;
 ReactDOM.render(<App />, document.getElementById("root"));
 https://github.com/vardhan2/React-Assignments-Set---Rendering-Multiple-Components-with-React/
